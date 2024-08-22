61c14a185f74b253f0269c3fc9e4659748a753f2
Khanh Nguyen <khanh.nguyenduy@axonactive.com>
2024-08-22 09:35:37 +0700

    Update createPrsToGenerateReleaseDrafterWorkflow.sh

--- createPrsToGenerateReleaseDrafterWorkflow.sh
+++ createPrsToGenerateReleaseDrafterWorkflow.sh
@@ -7,7 +7,6 @@
 # Using https://cli.github.com/
 
 org="axonivy-market"
-user="ndkhanh-axonivy"
 
 ignored_repos=(
   "market-up2date-keeper"
@@ -49,27 +48,18 @@ jobs:
       prerelease: \${{ github.event.inputs.prerelease }}"
 
 githubRepos() {
-  # ghApi="orgs/${org}/repos?per_page=100"
-  ghApi="users/${user}/repos?per_page=100"
+  ghApi="orgs/${org}/repos?per_page=100"
   gh api "${ghApi}"
 }
 
-githubReposC(){
-  cache="/tmp/gh-${org}.json"
-  if [ ! -f "${cache}" ]; then
-    githubRepos > "${cache}"
-  fi
-  cat "${cache}"
-}
-
 collectRepos() {
-  githubReposC | 
+  githubRepos | 
     jq -r '.[] | 
     select(.archived == false) | 
     select(.is_template == false) | 
     select(.default_branch == "master") | 
     select(.language != null) | 
-      .name'
+      .name' | sed 's/\r//g'
 }
 
 create_label_if_not_exists() {
@@ -78,11 +68,11 @@ create_label_if_not_exists() {
   label_color="e11d21"  # Optional: You can choose a color for the label
 
   # Check if the label exists
-  label_exists=$(gh api repos/${user}/${repo_name}/labels --jq '.[] | select(.name == "'"$label_name"'") | .name')
+  label_exists=$(gh api repos/${org}/${repo_name}/labels --jq '.[] | select(.name == "'"$label_name"'") | .name')
 
   if [ -z "$label_exists" ]; then
     echo "Creating label '$label_name' in repository $repo_name"
-    gh api repos/${user}/${repo_name}/labels -X POST -f name="$label_name" -f color="$label_color"
+    gh api repos/${org}/${repo_name}/labels -X POST -f name="$label_name" -f color="$label_color"
   else
     echo "Label '$label_name' already exists in repository $repo_name"
   fi
@@ -97,7 +87,10 @@ create_pr() {
     return
   fi
 
-  git clone "https://github.com/${user}/${repo_name}.git"
+  # Ensure repo name has no carriage return characters
+  repo_name=$(echo "$repo_name" | sed 's/\r//g')
+
+  git clone "https://github.com/${org}/${repo_name}.git"
   cd "${repo_name}"
 
   # Define branch and PR variables
@@ -112,14 +105,11 @@ create_pr() {
     git checkout -b "$branch_name"
   fi
 
-  if [ ! -f "$workflow_path" ]; then
-    mkdir -p .github/workflows
-    echo "$workflow_content" > "$workflow_path"
-    git add "$workflow_path"
-    git commit -m "Add release-drafter workflow"
-  else
-    echo "Workflow file already exists in branch $branch_name"
-  fi
+  # Always override the existing workflow file with new content
+  mkdir -p .github/workflows
+  echo "$workflow_content" > "$workflow_path"
+  git add "$workflow_path"
+  git commit -m "Update release-drafter workflow"
 
   git push origin "$branch_name"
 

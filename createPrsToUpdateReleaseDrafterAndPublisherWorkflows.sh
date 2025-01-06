7cfc5c37360a60f75d1f516c4b36581ebf1dbaaf
Khanh Nguyen <khanh.nguyenduy@axonactive.com>
2025-01-06 19:22:24 +0700

    Update createPrsToUpdateReleaseDrafterAndPublisherWorkflows.sh

--- createPrsToUpdateReleaseDrafterAndPublisherWorkflows.sh
+++ createPrsToUpdateReleaseDrafterAndPublisherWorkflows.sh
@@ -10,12 +10,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . ${DIR}/repo-collector.sh
 
 create_label_if_not_exists() {
-  repo_name=$1
-  label_name="skip-changelog"
-  label_color="e11d21"
+  local repo_name="$1"
+  local label_name="skip-changelog"
+  local label_color="e11d21"
 
-  # Check if the label exists
-  label_exists=$(gh api repos/${org}/${repo_name}/labels --jq '.[] | select(.name == "'"$label_name"'") | .name')
+  local label_exists=$(gh api repos/${org}/${repo_name}/labels --jq '.[] | select(.name == "'"$label_name"'") | .name')
 
   if [ -z "$label_exists" ]; then
     echo "Creating label '$label_name' in repository $repo_name"
@@ -25,26 +24,12 @@ create_label_if_not_exists() {
   fi
 }
 
-create_pr() {
-  repo_name=$1
+process_branch() {
+  local repo_name="$1"
+  local branch_name="$2"
+  local base_branch="$3"
 
-  if [[ " ${ignored_repos[@]} " =~ " ${repo_name} " ]]; then
-    echo "Ignoring repo ${repo_name}"
-    return
-  fi
-
-  echo "Processing repository $repo_name"
-  # Ensure repo name has no carriage return characters
-  repo_name=$(echo "$repo_name" | sed 's/\r//g')
-  branch_name="feature/MARP-1053-Update-release-drafter-and-publisher-workflows"
-  pr_title="MARP-1053 Update release-drafter and release-publisher workflows"
-  workflow_path_release_drafter=".github/workflows/release-drafter.yml"
-  workflow_path_publish_release_drafter=".github/workflows/publish-release-drafter.yml"
-  workflow_content_release_drafter=$(fetch_raw_file "$org" "market-product" "$workflow_path_release_drafter")
-  workflow_content_publish_release_drafter=$(fetch_raw_file "$org" "market-product" "$workflow_path_publish_release_drafter")
-
-  git clone "https://github.com/${org}/${repo_name}.git"
-  cd "${repo_name}"
+  echo "Processing branch $branch_name in repository $repo_name"
 
   if git ls-remote --heads origin "$branch_name" | grep -q "$branch_name"; then
     echo "Branch $branch_name already exists in $repo_name"
@@ -53,42 +38,52 @@ create_pr() {
     git checkout -b "$branch_name"
   fi
 
-  # Always override the existing workflow file with new content
   mkdir -p .github/workflows
-  echo "$workflow_content_release_drafter" > "$workflow_path_release_drafter"
-  git add "$workflow_path_release_drafter"
-  
-  echo "$workflow_content_publish_release_drafter" > "$workflow_path_publish_release_drafter"
-  git add "$workflow_path_publish_release_drafter"
+  echo "$workflow_content_release_drafter" > .github/workflows/release-drafter.yml
+  git add .github/workflows/release-drafter.yml
 
-  # Delete old workflow file if it exists
-  old_workflow_path_publish_release=".github/workflows/publish-release.yml"
-  if [ -f "$old_workflow_path_publish_release" ]; then
-    rm "$old_workflow_path_publish_release"
-    git add "$old_workflow_path_publish_release"
-  fi
+  echo "$workflow_content_publish_release_drafter" > .github/workflows/publish-release-drafter.yml
+  git add .github/workflows/publish-release-drafter.yml
 
-  git commit -m "Updaate publish-release-drafter and release-drafter workflows"
+  git commit -m "Update workflows for branch $branch_name"
   git push origin "$branch_name"
 
-  pr_id=$(gh pr list --head "$branch_name" --base master --json number --jq '.[0].number')
+  local pr_id=$(gh pr list --head "$branch_name" --base "$base_branch" --json number --jq '.[0].number')
   if [ -z "$pr_id" ]; then
     echo "Creating a pull request"
-    gh pr create --title "$pr_title" --body "This PR adds the Release Drafter workflow to the repository." --base master --head "$branch_name"
-    pr_id=$(gh pr list --head "$branch_name" --base master --json number --jq '.[0].number')
+    gh pr create --title "MARP-1053: Update workflows for branch $branch_name" --body "This PR updates the workflows for branch $branch_name." --base "$base_branch" --head "$branch_name"
   else
     echo "Pull request already exists for branch $branch_name"
   fi
+}
 
-  if [ -n "$pr_id" ]; then
-    pr_labels=$(gh pr view "$pr_id" --json labels --jq '.labels[].name' | grep -w "skip-changelog")
-    if [ -z "$pr_labels" ]; then
-      echo "Adding label to the pull request"
-      gh pr edit "$pr_id" --add-label "skip-changelog"
-    else
-      echo "Label 'skip-changelog' already present on PR $pr_id"
-    fi
+create_pr() {
+  local repo_name="$1"
+
+  git clone "https://github.com/${org}/${repo_name}.git"
+  cd "${repo_name}"
+
+  local branches=$(git branch -r | grep "release/" | sed 's|origin/||g')
+  workflow_path_release_drafter=".github/workflows/release-drafter.yml"
+  workflow_path_publish_release_drafter=".github/workflows/publish-release-drafter.yml"
+  workflow_content_release_drafter=$(fetch_raw_file "$org" "market-product" "$workflow_path_release_drafter")
+  workflow_content_publish_release_drafter=$(fetch_raw_file "$org" "market-product" "$workflow_path_publish_release_drafter")
+
+  if [[ ! " $branches " =~ " master " ]]; then
+    branches+=" master"
   fi
+
+  for base_branch in $branches; do
+    local branch_name=""
+    if [[ $base_branch == "master" ]]; then
+      branch_name="feature/MARP-1053"
+    else
+      local release_version=$(echo "$base_branch" | sed 's|release/||')
+      branch_name="feature/${release_version}/MARP-1053"
+    fi
+
+    process_branch "$repo_name" "$branch_name" "$base_branch"
+  done
 
   cd ..
   rm -rf "${repo_name}"
@@ -105,9 +100,13 @@ fetch_raw_file() {
 
 main() {
   echo "====== Starting script ======"
-  collectRepos | while read -r repo_name; do
-    create_label_if_not_exists "$repo_name"
-    create_pr "$repo_name"
+  collectRepos | sed '/^$/d' | while read -r repo_name; do
+    if [ "$repo_name" == "open-weather-connector" ]; then
+      create_label_if_not_exists "$repo_name"
+      create_pr "$repo_name"
+    else
+      echo "Skipping repository: $repo_name"
+    fi
   done
   echo "====== End script ======"
 }

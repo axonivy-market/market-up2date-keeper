147df91136ff5eeec17d56f199b864b8e774f953
Khanh Nguyen <khanh.nguyenduy@axonactive.com>
2025-01-06 18:47:47 +0700

    Update createPrsToUpdateReleaseDrafterAndPublisherWorkflows.sh

--- createPrsToUpdateReleaseDrafterAndPublisherWorkflows.sh
+++ createPrsToUpdateReleaseDrafterAndPublisherWorkflows.sh
@@ -1,93 +1,18 @@
 #!/bin/bash
 
-# Release Drafter Workflow Creator CLI
+# Release Drafter Workflow Modifier CLI
 # ===================================
-# This script creates pull requests to add a Release Drafter workflow to each repository
+# This script creates pull requests to update a Release Drafter workflow to each repository
 # in the axonivy-market GitHub Organization.
 # Using https://cli.github.com/
 
-org="axonivy-market"
-
-ignored_repos=(
-  "market-up2date-keeper"
-  "market.axonivy.com"
-  "market-monitor"
-  "market"
-  "demo-projects"
-)
-
-workflow_file_release_drafter=".github/workflows/release-drafter.yml"
-workflow_content_release_drafter="name: Release Drafter
-
-on:
-  push:
-    branches:
-      - master
-      - release/*
-  pull_request:
-    types: [opened, reopened, synchronize]
-
-jobs:
-  release_drafter:
-    uses: axonivy-market/github-workflows/.github/workflows/release-drafter.yml@v5"
-
-workflow_file_publish_release=".github/workflows/publish-release-drafter.yml"
-workflow_content_publish_release="name: Publish Release Drafter
-
-on:
-  workflow_run:
-    workflows: ["Release-Build"]
-    types:
-      - completed
-
-jobs:
-  publish_release_drafter:
-    if: ${{ github.event.workflow_run.conclusion == 'success' }}
-    uses: axonivy-market/github-workflows/.github/workflows/publish-release-drafter.yml@v5
-    # The following input parameters are available:
-    #
-    # - tag: (optional) Specifies the release tag (e.g., 'v10.0.0').
-    #        If not provided, the most recent tag in the repository will be used by default.
-    #        This ensures the workflow runs without requiring explicit tag input.
-    #
-    # - commitish: (optional) Indicates the branch name for the release.
-    #              If not provided, it defaults to 'master'. For versioned releases, 
-    #              the workflow automatically checks for a corresponding branch 
-    #              (e.g., 'release/10.0' for version 10.0) and uses it if available.
-    #              This allows the workflow to execute with default branch behavior.
-    #
-    # - publish: (optional) Controls whether the release is published automatically.
-    #            Defaults to 'true'. Set to 'false' to create a draft release 
-    #            without immediate publication.
-    #
-    # Note: Both 'tag' and 'commitish' are configured to run with sensible defaults,
-    #       so manual input is not required unless custom values are needed.
-    #
-    # Uncomment and modify the inputs as needed:
-    # with:
-    #   tag: v10.0.0
-    #   commitish: release/10.0
-    #   publish: false"
-
-githubRepos() {
-  ghApi="orgs/${org}/repos?per_page=100"
-  gh api "${ghApi}"
-}
-
-collectRepos() {
-  githubRepos | 
-    jq -r '.[] | 
-    select(.archived == false) | 
-    select(.is_template == false) | 
-    select(.default_branch == "master") | 
-    select(.language != null) | 
-      .name' | sed 's/\r//g'
-}
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. ${DIR}/repo-collector.sh
 
 create_label_if_not_exists() {
   repo_name=$1
   label_name="skip-changelog"
-  label_color="e11d21"  # Optional: You can choose a color for the label
+  label_color="e11d21"
 
   # Check if the label exists
   label_exists=$(gh api repos/${org}/${repo_name}/labels --jq '.[] | select(.name == "'"$label_name"'") | .name')
@@ -102,22 +27,24 @@ create_label_if_not_exists() {
 
 create_pr() {
   repo_name=$1
-  echo "Processing repository $repo_name"
 
   if [[ " ${ignored_repos[@]} " =~ " ${repo_name} " ]]; then
     echo "Ignoring repo ${repo_name}"
     return
   fi
 
+  echo "Processing repository $repo_name"
   # Ensure repo name has no carriage return characters
   repo_name=$(echo "$repo_name" | sed 's/\r//g')
+  branch_name="feature/MARP-1053-Update-release-drafter-and-publisher-workflows"
+  pr_title="MARP-1053 Update release-drafter and release-publisher workflows"
+  workflow_path_release_drafter=".github/workflows/release-drafter.yml"
+  workflow_path_publish_release_drafter=".github/workflows/publish-release-drafter.yml"
+  workflow_content_release_drafter=$(fetch_raw_file "$org" "market-product" "$workflow_path_release_drafter")
+  workflow_content_publish_release_drafter=$(fetch_raw_file "$org" "market-product" "$workflow_path_publish_release_drafter")
 
   git clone "https://github.com/${org}/${repo_name}.git"
   cd "${repo_name}"
-
-  # Define branch and PR variables
-  branch_name="feature/MARP-1053-Update-release-drafter-and-publisher-workflows"
-  pr_title="MARP-1053 Update release-drafter and release-publisher workflows"
 
   if git ls-remote --heads origin "$branch_name" | grep -q "$branch_name"; then
     echo "Branch $branch_name already exists in $repo_name"
@@ -128,24 +55,23 @@ create_pr() {
 
   # Always override the existing workflow file with new content
   mkdir -p .github/workflows
-  echo "$workflow_content_release_drafter" > "$workflow_file_release_drafter"
-  git add "$workflow_file_release_drafter"
+  echo "$workflow_content_release_drafter" > "$workflow_path_release_drafter"
+  git add "$workflow_path_release_drafter"
+  
+  echo "$workflow_content_publish_release_drafter" > "$workflow_path_publish_release_drafter"
+  git add "$workflow_path_publish_release_drafter"
 
   # Delete old workflow file if it exists
-  old_workflow_file_publish_release=".github/workflows/publish-release.yml"
-  if [ -f "$old_workflow_file_publish_release" ]; then
-    rm "$old_workflow_file_publish_release"
-    git add "$old_workflow_file_publish_release"
+  old_workflow_path_publish_release=".github/workflows/publish-release.yml"
+  if [ -f "$old_workflow_path_publish_release" ]; then
+    rm "$old_workflow_path_publish_release"
+    git add "$old_workflow_path_publish_release"
   fi
-  
-  echo "$workflow_content_publish_release" > "$workflow_file_publish_release"
-  git add "$workflow_file_publish_release"
-  git commit -m "Updaate publish-release-drafter and release-drafter workflows"
 
+  git commit -m "Updaate publish-release-drafter and release-drafter workflows"
   git push origin "$branch_name"
 
   pr_id=$(gh pr list --head "$branch_name" --base master --json number --jq '.[0].number')
-
   if [ -z "$pr_id" ]; then
     echo "Creating a pull request"
     gh pr create --title "$pr_title" --body "This PR adds the Release Drafter workflow to the repository." --base master --head "$branch_name"
@@ -168,12 +94,22 @@ create_pr() {
   rm -rf "${repo_name}"
 }
 
+fetch_raw_file() {
+  local owner="$1"
+  local repo="$2"
+  local file_path="$3"
+
+  gh api repos/$org/$repo/contents/$file_path?ref=feature/MARP-1053-Update-publish-release-template \
+    -H "Accept: application/vnd.github.v3.raw"
+}
+
 main() {
-  echo "Repositories found:"
+  echo "====== Starting script ======"
   collectRepos | while read -r repo_name; do
     create_label_if_not_exists "$repo_name"
     create_pr "$repo_name"
   done
+  echo "====== End script ======"
 }
 
 main

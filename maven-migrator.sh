f63dc455e62a0d2b32ef9abbaaf70eaeb7672c35
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2026-05-11 11:59:28 +0700

    Update maven-migrator.sh

--- maven-migrator.sh
+++ maven-migrator.sh
@@ -5,15 +5,18 @@ updateMvnProperty() {
 }
 
 artifactVersion() {
+  # if root pom.xml exists
   newVersion="$1"
   # if root pom.xml exists, update all declared modules in one pass
   if [ -f "pom.xml" ]; then
     mvn -B versions:set -DnewVersion=${newVersion} -DgenerateBackupPoms=false -DprocessAllModules=true
+    mvn -B versions:use-latest-versions -DgenerateBackupPoms=false -DprocessAllModules
   fi
   # update version in pom.xml
   # loop through all folders
   for d in */ ; do
     echo "Updating $d"
-    mvn -f "$d" -B versions:set -DnewVersion=${newVersion} -DgenerateBackupPoms=false
+    mvn -f $d -B versions:set -DnewVersion=$convert_to_version -DgenerateBackupPoms=false -DprocessAllModules=true
+    mvn -f $d -B versions:use-latest-versions -DgenerateBackupPoms=false -DprocessAllModules
   done
 }

f97bedf3eb8c003c5e50635be62320ec965d00e7
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2026-05-11 11:14:27 +0700

    Update maven-migrator.sh

--- maven-migrator.sh
+++ maven-migrator.sh
@@ -6,8 +6,10 @@ updateMvnProperty() {
 
 artifactVersion() {
   newVersion="$1"
+  oldVersion=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout 2>/dev/null)
+  echo "Updating maven version from ${oldVersion} to ${newVersion}"
   # if root pom.xml exists
   if [ -f "pom.xml" ]; then
-    mvn -B versions:set -DnewVersion=${newVersion} -DgenerateBackupPoms=false -DprocessAllModules=true
+    mvn -B versions:set -DoldVersion=${oldVersion} -DnewVersion=${newVersion} -DgenerateBackupPoms=false -DprocessAllModules=true
   fi
 }

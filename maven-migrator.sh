1b95427df97ad8a03f53d0ff5f4566671badcb3c
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2026-05-11 13:31:08 +0700

    wip

--- maven-migrator.sh
+++ maven-migrator.sh
@@ -8,7 +8,9 @@ artifactVersion() {
   newVersion="$1"
   # if root pom.xml exists
   if [ -f "pom.xml" ]; then
+    echo "Updating artifact version to ${newVersion}"
     mvn -B versions:set -DnewVersion=${newVersion} -DgenerateBackupPoms=false -DprocessAllModules=true
+    echo "Updating dependencies to latest versions"
     mvn -B versions:use-latest-versions -DgenerateBackupPoms=false -DprocessAllModules
   fi
 }

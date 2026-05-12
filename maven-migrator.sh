e1869e14220b72eab4bd2465266ce7a184e294cb
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2026-05-12 14:29:23 +0700

    Update maven-migrator.sh

--- maven-migrator.sh
+++ maven-migrator.sh
@@ -3,11 +3,30 @@ updateMvnProperty() {
   value="$2"
   mvn -B versions:set-property versions:commit "-Dproperty=${name}" "-DnewVersion=${value}" -DallowSnapshots=true -DprocessAllModules
 }
+removeMvnProperty() {
+  name="$1"
+
+  # Remove from root pom.xml
+  if [ -f "pom.xml" ]; then
+    echo "Removing property '$name' from root pom.xml"
+    sed -i "/<${name}>.*<\/${name}>/d" pom.xml
+  fi
+
+  # Remove from all child pom.xml files
+  for d in */ ; do
+    if [ -f "${d}pom.xml" ]; then
+      echo "Removing property '$name' from ${d}pom.xml"
+      sed -i "/<${name}>.*<\/${name}>/d" "${d}pom.xml"
+    fi
+  done
+}
 
 artifactVersion() {
   newVersion="$1"
   # if root pom.xml exists
   if [ -f "pom.xml" ]; then
+    removeMvnProperty "project.build.plugin.versions"
+    removeMvnProperty "tester.version"
     mvn -B versions:set -DnewVersion=${newVersion} -DgenerateBackupPoms=false -DprocessAllModules=true
     mvn -B versions:use-latest-versions -DgenerateBackupPoms=false -DprocessAllModules
   fi
